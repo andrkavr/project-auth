7557731c42067fd675379ea7330fe8a028aa1957
andrkavr <72137459+andrkavr@users.noreply.github.com>
2023-12-06 11:01:26 +0100

    added export to routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,22 +6,33 @@ const router = express.Router();
 
 // Route to get available endpoints
 router.get("/", (req, res) => {
-    const endpoints = listEndpoints(router);
-    res.json({endpoints})
+  const endpoints = listEndpoints(router);
+  res.json({ endpoints });
 });
 
+router.get("/hello", (req, res) => {
+  res.json("Hello, this is working");
+});
 
 router.post("/users", async (req, res) => {
-    try {
-        const {username, email, password} = req.body;
-        const user = new UserModel({email, username, password: bcrypt.hashSync(password)})
-        UserModel.save();
-        res.status(201).json({id:user._id, accessToken: user.accessToken})
-    }catch(err) {
-        res.status(400).json({
-            message: "Could not create user", errors: err.errors})
-    }
-})
+  try {
+    const { username, email, password } = req.body;
+    const user = new UserModel({
+      email,
+      username,
+      password: bcrypt.hashSync(password),
+    });
+    UserModel.save();
+    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (err) {
+    res.status(400).json({
+      message: "Could not create user",
+      errors: err.errors,
+    });
+  }
+});
 
 // Inte klart
-router.post("/authentication", async (req, res) => {})
+router.post("/authentication", async (req, res) => {});
+
+export default router;
